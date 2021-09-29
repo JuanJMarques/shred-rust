1fc9704ed9f1f4b20a648a5e8c3cca22a9617276
Juan Marqués <juanjemdc@gmail.com>
2021-09-29 20:55:32 +0200

    implement multi threading for shred method.
    feat: use the thread-pool for shredding the files with multiples threads.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::fs::OpenOptions;
 use std::path::Path;
+use std::sync::Arc;
 
 use clap::{App, Arg};
 
 use rand::Rng;
 
 use shred::files;
+use shred::threadpool::ThreadPool;
 
 mod utils;
 
@@ -17,7 +19,8 @@ fn main() {
                 .short("n")
                 .long("iterations")
                 .takes_value(true)
-                .help("Overwrite N times instead of the default (3)."),
+                .help("Overwrite N times.")
+                .default_value("3"),
         )
         .arg(
             Arg::with_name("remove")
@@ -54,34 +57,60 @@ fn main() {
                 .takes_value(true)
                 .help("Shred this many bytes (suffixes like K, M, G accepted)."),
         )
-        .arg(Arg::from_usage(
-            " <FILE>              'Sets the file to use'",
-        ))
+        .arg(Arg::with_name("threads")
+            .short("t")
+            .long("threads")
+            .takes_value(true)
+            .default_value("1")
+            .help("Number of threads to execute in parallel")
+        )
+        // .arg(Arg::from_usage(
+        //     " <FILE>              'Sets the file to use'",
+        // ))
+        .arg(
+            Arg::with_name("FILES")
+                .help("Sets the files to to shred")
+                .required(true)
+                .multiple(true)
+        )
         .get_matches();
     let times = matches
         .value_of("times")
-        .unwrap_or("3")
-        .parse::<i32>()
-        .unwrap_or_else(|err| {
-            panic!("! {:?}", err);
-        }) as u32;
+        .map(|s| s.parse::<u32>())
+        .unwrap().unwrap();
     let remove = matches.is_present("remove");
     let write_zeroes = matches.is_present("zero");
     let verbose = matches.is_present("verbose");
-    let file_path_arg = matches.value_of("FILE");
-    let file_path =
-        file_path_arg.unwrap_or_else(|| panic!("Cannot parse file! {:?}", file_path_arg));
     let recursive = matches.is_present("recursive");
-    let size_str = matches.value_of("size").unwrap_or("-1");
-    shred(
-        file_path,
-        times,
-        remove,
-        write_zeroes,
-        verbose,
-        recursive,
-        size_str,
-    )
+    let size_str = Arc::new(
+        String::from(
+            matches.value_of("size")
+                .map(|s| s)
+                .map(|s| s)
+                .unwrap_or("-1")
+        )
+    );
+    let threads = matches.value_of("threads")
+        .map(|s| s.parse::<usize>())
+        .unwrap().unwrap();
+    let thread_pool: ThreadPool = ThreadPool::new(threads);
+    if let Some(file_paths) = matches.values_of_lossy("FILES") {
+        for file_path in file_paths {
+            let size_str = Arc::clone(&size_str);
+            thread_pool.execute(move ||
+                shred(
+                    file_path.as_str(),
+                    times,
+                    remove,
+                    write_zeroes,
+                    verbose,
+                    recursive,
+                    size_str.as_str(),
+                )
+            );
+        }
+    }
+    // matches.is_present("test");
 }
 
 /// rewrites and optionally deletes the file pointed by `file_path`.
@@ -115,14 +144,14 @@ fn shred(
     let size_present = !"-1".eq_ignore_ascii_case(size_str);
     let path = Path::new(file_path);
     let file_metadata = path.metadata().unwrap_or_else(|err| {
-        panic!("! {:?}", err.kind());
+        panic!("{}! {:?}",file_path, err.kind());
     });
     if file_metadata.is_dir() && !recursive {
         panic!("cannot shred directories without the recursive parameter. See help");
     }
     if file_metadata.is_dir() {
         for entry in path.read_dir().unwrap_or_else(|err| {
-            panic!("! {:?}", err.kind());
+            panic!("{}! {:?}",file_path, err.kind());
         }) {
             let sub_file = entry.unwrap();
             shred(
